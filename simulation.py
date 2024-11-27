a8cdf961c704f3f5e5c1155149470a8f90da2d01
Maniiic <oliver.vaillantjones@gmail.com>
2024-11-27 21:33:25 +0000

    Draw food Test 1

--- simulation.py
+++ simulation.py
@@ -1,9 +1,11 @@
 # Imports
 import sys
 import pygame
+import random
 
 #Constants
 black = (0, 0, 0)
+yellow = (255,255,0)
 
 #Variables for pygame window
 res = pygame.Vector2(816,459) #16:9
@@ -12,7 +14,35 @@ clock = pygame.time.Clock()
 deltaTime = clock.tick(60)/1000
 backgroundColour = black
 
+#Constants
+foodAmount = 5
+margin = 50
+
+#Food and creatures will inherit from entity class
+class Entity:
+  def __init__(self, col, size):
+    #Spawn with a random position on the screen, 
+    self.pos = randomVector()
+    self.col = col
+    self.size = size
+    
+  def draw(self):
+    # Draws the objects on the pygame screen
+    pygame.draw.circle(surface, self.col, self.pos, self.size)
+
+class Food(Entity):
+  def __init__(self):
+    super().__init__(yellow, 10)
+
+def randomVector():
+  # Generates a random vector
+  return pygame.Vector2(random.randint(margin, int(res.x) - margin), random.randint(margin, int(res.y - margin)))
+
+#Initial lists
+foods = [Food() for x in range(foodAmount)]
+
 surface.fill(backgroundColour)
+
 #Main Loop
 while True:
   #Close window
